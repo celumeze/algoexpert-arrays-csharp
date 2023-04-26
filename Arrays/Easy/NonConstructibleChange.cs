b7e5e9ee364c218f6a98f8738d1f000f2cb1cc1e
Charles <charleselumeze@gmail.com>
2023-04-26 01:51:17 +0100

    added another easy solution

---
+++ Arrays/Easy/NonConstructibleChange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays.Easy
+{
+    public static class NonConstructibleChange
+    {
+        public static int GetMinimumUnavailableChange(int[] coins)
+        {
+                if(coins.Length == 0) return 1;            
+                int changeAvailable = 0;
+                Array.Sort(coins);
+
+                for(int i=0;i< coins.Length;i++)
+                {
+                        changeAvailable++;
+                        if (coins[i] <= changeAvailable) 
+                        {
+                           changeAvailable--;
+                           changeAvailable += coins[i];    
+                        }
+                        else
+                        {
+                            return changeAvailable;
+                        }                    
+                }
+                return ++changeAvailable;
+        }
+    }
+}
---
+++ Arrays/Easy/TournamentWinner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays.Easy
+{
+    public static  class TournamentWinner
+    {
+        public static string GetWinner(List<List<string>> competitions, List<int> results)
+        {
+            Dictionary<string, int> teamsPointsTotal = new Dictionary<string, int>();
+            int highestScore = 0;
+
+            for(int i=0; i<competitions.Count;i++)
+            {
+                string winner = results[i] == 1 ? (competitions[i])[0] : (competitions[i])[1];
+
+                if (!teamsPointsTotal.ContainsKey(winner))
+                {
+                    teamsPointsTotal.Add(winner, 3);
+                }
+                else
+                {
+                    teamsPointsTotal[winner] = Convert.ToInt32(teamsPointsTotal[winner]) + 3;
+                }
+
+                highestScore = teamsPointsTotal[winner] > highestScore ? teamsPointsTotal[winner] : highestScore;               
+            }
+            return teamsPointsTotal.Where(x => x.Value == highestScore).FirstOrDefault().Key;
+        }
+    }
+}
