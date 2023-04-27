5e75f6bf05d5dbebcf7814ea05d3564025a348b7
Charles <charleselumeze@gmail.com>
2023-04-27 22:02:32 +0100

    ThreeNumberSum solution added

---
+++ Arrays/Medium/ThreeNumberSum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays.Medium
+{
+    public static class ThreeNumberSum
+    {
+        public static List<int[]> ReturnThreeNumberSum(int[] array, int targetSum)
+        {
+            List<int[]> targetArrayList = new List<int[]>();
+            Array.Sort(array);
+            
+
+            for(int i=0;i<array.Length-2;i++)
+            {
+                int leftCounter = i + 1;
+                int rightCounter = array.Length - 1;
+                while (array[leftCounter] < array[rightCounter])
+                {
+                    int sum = array[i] + array[leftCounter] + array[rightCounter];
+                    if(sum < targetSum)
+                    {
+                        leftCounter++;
+                    }
+                    else if (sum > targetSum)
+                    {
+                        rightCounter--;
+                    }
+                    else
+                    {
+                        int[] targetArray = new int[3];
+                        targetArray[0] = array[i]; 
+                        targetArray[1] = array[leftCounter];
+                        targetArray[2] = array[rightCounter];
+                        targetArrayList.Add(targetArray);
+                        leftCounter++;
+                        rightCounter--;
+                    }
+                }
+            }
+
+           
+            return targetArrayList;
+        }
+    }
+}
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Arrays.Easy;
+using Arrays.Medium;
 ///Valid Subsequence
 //Console.WriteLine("-----Valid Subsequence-----");
 //int[] array = { 5, 1, 22, 25, 6, -1, 8, 10 };
 //int[] sequence = { 1, 6, -1, 10 };
 //Console.WriteLine(ValidSubsequence.IsValidSubsequence(array, sequence).ToString());
 //Console.WriteLine("-----Valid Subsequence-----");
+
+int[] array = { 1, 2, 3 };
+int targetSum = 6;
+
+ThreeNumberSum.ReturnThreeNumberSum(array, targetSum);
